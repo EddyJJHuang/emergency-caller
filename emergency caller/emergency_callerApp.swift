71489f19bc991f43ed01c4800d5668715ed2d6bc
Jiajun Huang <huangjiajun113@gmail.com>
2023-05-18 11:14:02 -0700

    Initial Commit

---
+++ emergency caller/emergency_callerApp.swift
@@ -0,0 +1,20 @@
+//
+//  emergency_callerApp.swift
+//  emergency caller
+//
+//  Created by 黄家骏 on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct emergency_callerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ emergency callerUITests/emergency_callerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  emergency_callerUITestsLaunchTests.swift
+//  emergency callerUITests
+//
+//  Created by 黄家骏 on 5/18/23.
+//
+
+import XCTest
+
+final class emergency_callerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
