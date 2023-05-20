edb7a78e39c03fe7e1a1c8c6be66ca1d99dab2f7
Jiawen <nkzhang@ucdavis.edu>
2023-05-20 14:05:17 -0700

    personal information (name, phone, birthday)

--- emergency caller/ContentView.swift
+++ emergency caller/ContentView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 import DynamicColor
 
+
 struct SizingView: View {
     @State private var showPersonalInfo = false // State variable to track whether to show the personal info view
 
@@ -50,30 +51,73 @@ struct SizingView: View {
     }
 }
 
-struct PersonalInfoView: View {
-    @Environment(\.presentationMode) private var presentationMode // Environment variable to dismiss the sheet
 
+
+
+//personal info stack
+struct PersonalInfoView: View {
+    @Environment(\.presentationMode) private var presentationMode
+    
+    @State private var name: String = ""
+    @State private var phone: String = ""
+    @State private var birthdate = Date()
+    
+    private let dateRange: ClosedRange<Date> = {
+        let calendar = Calendar.current
+        let currentDate = Date()
+        let startDate = calendar.date(byAdding: .year, value: -100, to: currentDate)!
+        let endDate = currentDate
+        return startDate...endDate
+    }()
+    
     var body: some View {
-        VStack {
-            Text("Enter Personal Information")
-                .font(.headline)
-                .padding()
-            
-            // Add your personal information input fields here
-            
-            Button("Save") {
-                // Handle the save action here
+        NavigationView {
+            Form {
+                Section(header: Text("Enter Personal Information").font(.headline)) {
+                    TextField("Name", text: $name)
+                    TextField("Phone Number", text: $phone)
+                }
                 
-                presentationMode.wrappedValue.dismiss() // Dismiss the sheet
+                Section(header: Text("Birthdate").font(.headline)) {
+                    ScrollView(.horizontal) {
+                        HStack {
+                            Spacer()
+                            DatePicker("", selection: $birthdate, in: dateRange, displayedComponents: [.date])
+                                .datePickerStyle(WheelDatePickerStyle())
+                                .frame(width: 250) // Adjust the width as needed
+                            Spacer()
+                        }
+                    }
+                }
+                
+                Section {
+                    Button("Save") {
+                        savePersonalInfo()
+                    }
+                }
             }
-            .padding()
+            .navigationBarTitle("Personal Information")
+            .navigationBarItems(trailing: Button("Cancel") {
+                presentationMode.wrappedValue.dismiss()
+            })
         }
-        .navigationBarTitle("Personal")
+    }
+    
+    private func savePersonalInfo() {
+        // Here you can perform the necessary actions to save the user's personal information
+        // For example, you could store the values in a database or update a user profile
+        
+        // After saving, dismiss the view
+        presentationMode.wrappedValue.dismiss()
     }
 }
+
+
 
 struct Previews_SizingView_Previews: PreviewProvider {
     static var previews: some View {
         SizingView()
     }
 }
+
+
